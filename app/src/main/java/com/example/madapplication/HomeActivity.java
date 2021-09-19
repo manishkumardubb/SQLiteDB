221f715c82f1765d0fc9c241ed88e3a0a668ca15
manishkumardubb <manishkumardubb@pec.edu>
2021-09-19 22:27:14 +0530

    update1

--- app/src/main/java/com/example/madapplication/HomeActivity.java
+++ app/src/main/java/com/example/madapplication/HomeActivity.java
@@ -1,8 +1,16 @@
 package com.example.madapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.share){
+
+            ApplicationInfo api= getApplicationContext().getApplicationInfo();
+            String apkpath = api.sourceDir;
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("application/vnd.android.package-archive");
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(apkpath)));
+            startActivity(Intent.createChooser(intent,"share via"));
+        }return true;
     }
 }
